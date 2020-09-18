913ed59130dfb13036dd121642fa881b576044f9
LeeOuHyoun <leeouhyoun@gmail.com>
2020-09-18 19:37:59 +0900

    first push

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier'],
+  globals: {
+    _: true,
+    moment: true,
+  },
+  // add your custom rules here
+  rules: {
+    'no-console': 'off',
+  },
+}
---
+++ pages/pugTest/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #pugTest
+    .helloTest {{ name }}
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'hello pug',
+    }
+  },
+}
+</script>
+<style lang="scss">
+#pugTest {
+  .helloTest {
+    background-color: red;
+  }
+}
+</style>
