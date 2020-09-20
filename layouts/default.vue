6ae418a946085b4b5e7ee4aea94697d5c16354c8
LeeOuHyoun <leeouhyoun@gmail.com>
2020-09-21 01:56:55 +0900

    push

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,23 @@
 <template lang="pug">
   v-app()
-    v-app-bar.d-flex.justify-end(fixed app)
+    v-app-bar(fixed app)
+      nuxt-link.d-flex.link(tag="div" to="/")
+        v-img(
+          :src="require('@/assets/img/nobu.jpg')"
+          max-width="60px"
+          max-height="60px"
+        )
+        v-toolbar-title.px-2.d-flex.flex-column.justify-center
+          div.top 10%だけ頂いてやる
+          div.bottom nobusama
+      v-spacer
       v-btn.mx-1(small color="primary" outlined) ログイン
       v-btn.mx-1(small color="primary") 会員登録
     v-main
       v-container
         nuxt
     v-footer(:absolute="true" app)
-      span &copy; {{ $moment().format('YYYY') }}
+      span &copy; 2020 ~ 2020 nobusama Inc.
 </template>
 
 <script>
@@ -15,5 +25,25 @@ export default {
   data() {
     return {}
   },
+  methods: {},
 }
 </script>
+<style lang="scss" scoped>
+.link {
+  cursor: pointer;
+}
+.v-toolbar__title {
+  .top {
+    color: #333333;
+    font-size: 0.7rem;
+  }
+  .bottom {
+    line-height: 1rem;
+  }
+}
+.theme--light.v-footer {
+  background-color: #363634;
+  color: #fff;
+  font-size: 0.9rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,103 +1,32 @@
-<template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center"><logo /> <vuetify-logo /></div>
-      <v-card>
-        <v-card-title class="headline">
-          <div>
-            <p>
-              <strong style="color: #1697f6"
-                >Welcome to the Nobusama's World!!!</strong
-              >
-            </p>
-            <p>The Vuetify + Nuxt.js template</p>
-          </div>
-        </v-card-title>
-        <v-card-text>
-          <p><nuxt-link to="/pugTest">pugTestPage</nuxt-link></p>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+<template lang="pug">
+  v-layout
+    .d-flex.flex-wrap
+      nuxt-link.categories.mx-1.my-1(
+        to="/pugTest" tag="div"
+        v-for="(category, index) in categories" :key="index"
+      ) web
 </template>
-
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  data() {
+    return {
+      categories: [],
+    }
   },
   created() {
-    // const foo = { bar: 'baz' }
-    // console.log(_.get(foo, 'bar'))
-    // console.log(moment().format('yyyy-mm-dd'))
+    this.categories = new Array(30)
   },
 }
 </script>
+<style lang="scss">
+.categories {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 120px;
+  height: 110px;
+  border-radius: 7px;
+  background-color: #e9f6fc;
+  cursor: pointer;
+}
+</style>
